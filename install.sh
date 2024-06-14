3e8c7b24833a90920cdeb4e5e403eaa9bf9a7616
MehdiFTAYMIA <86959124+MehdiFTAYMIA@users.noreply.github.com>
2024-06-14 14:09:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Mise à jour des paquets et installation des dépendances
-sudo apt-get update && sudo apt-get install -y ca-certificates apt-transport-https
+apt-get update && sudo apt-get install -y ca-certificates apt-transport-https
 
 # Téléchargement et installation de libssl1.1
-sudo wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 
 # Ajout des dépôts Unifi
 echo 'deb [ arch=amd64,arm64 ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
+wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
 
 # Ajout des clés GPG MongoDB
 wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc | sudo apt-key add -
 echo "deb [trusted=yes] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 
 # Mise à jour des paquets après l'ajout des dépôts
-sudo apt-get update
+apt-get update
 
 # Installation de Unifi
-sudo apt-get install -y unifi
+apt-get install -y unifi
